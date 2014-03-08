79d195b81ae0ca3bb3e1f440fff3a80a27e204c1
VIctoria Payne <vhpayne@gmail.com>
2014-03-08 00:45:34 -0800

    Closing for the night, working homepage and post, but need to figure out xml requests

--- app.py
+++ app.py
@@ -6,8 +6,20 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/", methods=["POST"])
+def search_data():
+	city = request.form.get("search")
+	checkin = request.form.get("checkin")
+	checkout = request.form.get("checkout")
+	print city
+	print checkin
+	print checkout
+	return redirect(url_for("index"))
 
-
+@app.route("/search")
+def expedia_api():
+	print "I tried to go to the expedia api"
+	return redirect("http://api.ean.com")
 
 if __name__ == "__main__":
 	app.run(debug = True)
