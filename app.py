4e82f16be2bdeffa2834bd6be03cb1f8d9942e36
VIctoria Payne <vhpayne@gmail.com>
2014-03-08 18:41:19 -0800

    Search now works for dates in cities.  Still trying to unpack the response object.

--- app.py
+++ app.py
@@ -12,19 +12,22 @@ def search_data():
 	city = request.form.get("search")
 	checkin = request.form.get("checkin")
 	checkout = request.form.get("checkout")
-	return redirect(url_for("search_results", city=city))
+	return redirect(url_for("search_results", city=city, checkin=checkin, checkout=checkout))
 
-@app.route("/search/<city>")
-def search_results(city):
-	# xml_request = "<HotelListRequest><destinationstring>"+city+"</destinationstring><arrivalDate>"+checkin+"</arrivalDate><departureDate>"+checkout+"</departureDate></HotelListRequest>"
-	# payload = {"cid": "55505", "minorRev": "99", 
-	# 		"apiKey": "pnqbxpnwvest5ap5qrry4pk8", 
-	# 		"locale": "en_US", "currencyCode": "USD",
-	# 		"xml": xml_request}
-	# r = requests.get("http://api.ean.com/ean-services/rs/hotel/v3/list?", params=payload)
-	# print r.json
-	# hotel_json = r.requests.json
-	return render_template("search.html", city=city)
+@app.route("/search")
+def search_results():
+	city = request.args.get("city")
+	checkin = request.args.get("checkin")
+	checkout = request.args.get("checkout")
+	xml_request = "<HotelListRequest><destinationstring>"+city+"</destinationstring><arrivalDate>"+checkin+"</arrivalDate><departureDate>"+checkout+"</departureDate></HotelListRequest>"
+	payload = {"cid": "55505", "minorRev": "99", 
+	 		"apiKey": "pnqbxpnwvest5ap5qrry4pk8", 
+	 		"locale": "en_US", "currencyCode": "USD",
+	 		"xml": xml_request}
+	r = requests.get("http://api.ean.com/ean-services/rs/hotel/v3/list?", params=payload)
+	print r.json
+	hotel_list = r.json
+	return render_template("search.html", city=city, checkin=checkin, checkout=checkout, hotel_list=hotel_list)
 
 if __name__ == "__main__":
 	app.run(debug = True)
