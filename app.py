5af8858c68d2df763438f5c050ff75eb76b72fa1
VIctoria Payne <vhpayne@gmail.com>
2014-03-08 18:24:32 -0800

    Some work on the search, wanted to save since URL is working now

--- app.py
+++ app.py
@@ -12,21 +12,19 @@ def search_data():
 	city = request.form.get("search")
 	checkin = request.form.get("checkin")
 	checkout = request.form.get("checkout")
-	xml_request = "<HotelListRequest><destinationstring>"+city+"</destinationstring><arrivalDate>"+checkin+"</arrivalDate><departureDate>"+checkout+"</departureDate></HotelListRequest>"
-	payload = {"cid": "55505", "minorRev": "99", 
-			"apiKey": "pnqbxpnwvest5ap5qrry4pk8", 
-			"locale": "en_US", "currencyCode": "USD",
-			"xml": xml_request}
-	r = requests.get("http://api.ean.com/ean-services/rs/hotel/v3/list?", params=payload)
-	print r.url
-	r = r.json()
-	# return redirect("/")
-	return redirect(r.url)
+	return redirect(url_for("search_results", city=city))
 
-@app.route("/search")
-def expedia_api():
-	print "I tried to go to the expedia api"
-	return redirect("http://api.ean.com")
+@app.route("/search/<city>")
+def search_results(city):
+	# xml_request = "<HotelListRequest><destinationstring>"+city+"</destinationstring><arrivalDate>"+checkin+"</arrivalDate><departureDate>"+checkout+"</departureDate></HotelListRequest>"
+	# payload = {"cid": "55505", "minorRev": "99", 
+	# 		"apiKey": "pnqbxpnwvest5ap5qrry4pk8", 
+	# 		"locale": "en_US", "currencyCode": "USD",
+	# 		"xml": xml_request}
+	# r = requests.get("http://api.ean.com/ean-services/rs/hotel/v3/list?", params=payload)
+	# print r.json
+	# hotel_json = r.requests.json
+	return render_template("search.html", city=city)
 
 if __name__ == "__main__":
 	app.run(debug = True)
