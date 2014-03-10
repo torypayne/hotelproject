46dab74ce1a658fc77b1aa61a960f79f0d8b6e50
VIctoria Payne <vhpayne@gmail.com>
2014-03-10 00:37:24 -0700

    Improved results page

--- learningdict.py
+++ learningdict.py
@@ -5,7 +5,7 @@ from pprint import pprint
 city = "Paris"
 checkin = "4/7/2014"
 checkout = "4/9/2014"
-xml_request = "<HotelListRequest><destinationstring>"+city+"</destinationstring><arrivalDate>"+checkin+"</arrivalDate><departureDate>"+checkout+"</departureDate></HotelListRequest>"
+xml_request = "<HotelListRequest><destinationstring>"+city+"</destinationstring><arrivalDate>"+checkin+"</arrivalDate><departureDate>"+checkout+"</departureDate></HotelListRequest><numberOfResults>25</numberOfResults>"
 payload = {"cid": "55505", "minorRev": "99", 
 	 		"apiKey": "pnqbxpnwvest5ap5qrry4pk8", 
 	 		"locale": "en_US", "currencyCode": "USD",
