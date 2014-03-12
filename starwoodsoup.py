7af851021bca77b88aea89341ee3dbed49b1a05e
VIctoria Payne <vhpayne@gmail.com>
2014-03-11 17:38:24 -0700

    Starwood webscraping complete.  Marriot and Hilton still to go

--- starwoodsoup.py
+++ starwoodsoup.py
@@ -1,36 +1,63 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 Hotel_dictionary = {}
+
+r  = requests.get("http://www.starwoodhotels.com/preferredguest/account/starpoints/categories/popup.html?spgCategoryCode=7")
+
 
 for i in range(1,8):
 	print i
 	r  = requests.get("http://www.starwoodhotels.com/preferredguest/account/starpoints/categories/popup.html?spgCategoryCode="+str(i))
 	point_list = [0,3000,4000,7000,10000,12000,20000,30000]
 	high_season_points_list = [0,3000,4000,7000,10000,16000,25000,35000]
-	cashofcandp_list =
-	pointsofcandp_list = 
+	cashofcandp_list = [0, 30, 35, 55, 75, 100, 180, 275]
+	pointsofcandp_list = [0, 1500, 2000, 3500, 5000, 6000, 10000, 15000]
+	weekeendpoint_list = [0,2000,3000,7000,10000,12000,20000,30000]
+	if i <= 2:
+		weekendpossible = True
+	else:
+		weekendpossible = False
+	if i >= 5:
+		highseasonpossible = True
+	else:
+		highseasonpossible = False
 	data = r.text
 	soup = BeautifulSoup(data)
-	links_div = soup.findAll('div', attrs={'class':'hbGpInfoColumn hbGpClmn32'})
-	for div in links_div:
-		links = div.findAll('a')
+	table_row = soup.findAll('tr')
+	for row in table_row:
+		links = row.findAll('a')
 		for a in links:
 			# print a.contents[0]
 			try:
 				hotel = a.contents[0].encode('latin-1')
 				Hotel_dictionary[hotel] = {}
-				Hotel_dictionary[hotel]["link"] = a["href"]
-				Hotel_dictionary[hotel]["loyalty"] = "Hyatt"
+				jslink = a["href"]
+				jslink = re.search("location.href='(.*)'; }", jslink)
+				jslink = jslink.group(1)
+				Hotel_dictionary[hotel]["link"] = "http://www.starwoodhotels.com"+jslink	
+				Hotel_dictionary[hotel]["loyalty"] = "Starwood"
 				Hotel_dictionary[hotel]["category"] = i
 				Hotel_dictionary[hotel]["points"] = point_list[i]
-				Hotel_dictionary[hotel]["fifthfree"] = False
-				Hotel_dictionary[hotel]["cashandpoints"] = False
-				Hotel_dictionary[hotel]["cashofcandp"] = None
-				Hotel_dictionary[hotel]["pointsofcandp"] = None
-				Hotel_dictionary[hotel]["highseasonrate"] = point_list[i]
-				Hotel_dictionary[hotel]["highseasondates"] = None
-				Hotel_dictionary[hotel]["weekendrateapplies"] = False
-				Hotel_dictionary[hotel]["weekendpointrate"] = point_list[i]
+				Hotel_dictionary[hotel]["fifthfree"] = True
+				Hotel_dictionary[hotel]["cashandpoints"] = True
+				Hotel_dictionary[hotel]["cashofcandp"] = cashofcandp_list[i]
+				Hotel_dictionary[hotel]["pointsofcandp"] = pointsofcandp_list[i]
+				Hotel_dictionary[hotel]["highseasonapplies"] = highseasonpossible
+				if highseasonpossible == True:
+					highseasondates = []
+					season_div = row.findAll('div', attrs={'id':'seasonInfo'})
+					for div in season_div:
+						highdivs = div.findAll('div')
+						for date in highdivs:
+							highseasondates.append(date.contents[0].encode('latin-1'))
+					Hotel_dictionary[hotel]["highseasondates"] = highseasondates
+					highseasondates = []
+				Hotel_dictionary[hotel]["highseasonrate"] = high_season_points_list[i]
+				Hotel_dictionary[hotel]["weekendrateapplies"] = weekendpossible
+				Hotel_dictionary[hotel]["weekendpointrate"] = weekeendpoint_list[i]
 			except:
 				print "Add manually "+a.contents[0]
+
+# print Hotel_dictionary
